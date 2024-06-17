53bd64a3584228de817526beec7b113af480cc76
rahulgubili <rahul.gubiligari@paypay-corp.co.jp>
2024-06-17 18:17:54 +0900

    First Commit for Order service

---
+++ src/main/kotlin/com/online/store/application/entity/Orders.kt
@@ -0,0 +1,28 @@
+/*
+package com.online.store.application.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import lombok.AllArgsConstructor
+import lombok.Getter
+import lombok.NoArgsConstructor
+import lombok.Setter
+
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+data class Orders(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, updatable = false)
+    val id: Long? = null,
+
+)
+*/
